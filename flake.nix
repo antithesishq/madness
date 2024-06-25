186e88d188273989b683e848970237d505b3710f
Will Wilson <will.wilson@antithesis.com>
2024-06-25 14:25:32 -0400

    Flake description.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "TODO";
+  description = "Madness is a meta-loader for ELF binaries. It enables you to run programs built for non-Nix systems on NixOS.";
 
   outputs = _: {
     nixosModules.madness = import ./modules;
